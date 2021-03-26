bda1ac6e90cc22b6a7ad6716bd6c56331dec56f9
Mystical Apple <timon.cappellapimor@gmail.com>
2021-03-26 01:38:05 -0700

    Building bot

---
+++ bot.py
@@ -0,0 +1,40 @@
+# bot.py
+import os
+import discord
+
+TOKEN = os.environ['DISCORD_TOKEN']
+
+intents = discord.Intents.all()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+	print('Ready!')
+
+@client.event
+async def on_message(message):
+	ignoreSymbols = '!,.?'
+	response = 'General Kenobi!'
+
+	text = message.content.lower()
+	for symbol in text:
+		if symbol in ignoreSymbols:
+			text = text.replace(symbol, '')
+
+	if (text == "hello there"):
+		await message.channel.send(response)
+		print (f'In {message.guild} #{message.channel}:\n   {message.author} ᴬᴷᴬ {message.author.display_name} >>> {message.content}\n   {client.user} ᴬᴷᴬ {client.user.display_name} >>> {response}\n\n')
+
+	if message.content.startswith('pfp'):
+
+		requestedRawString = message.content[4:]
+
+		requestedID = discord.utils.get(client.get_all_members(), display_name=requestedRawString).id
+
+		requestedUser = client.get_user(requestedID)
+
+		await message.channel.send(f'{requestedUser.name} has the following profile picture:')
+		await message.channel.send(requestedUser.avatar_url)
+		print (f'{message.author} requested the pfp of {requestedRawString}\n\n')
+client.run(TOKEN)
