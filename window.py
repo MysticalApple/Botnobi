cff03258da25c3b199aff032c8e23bcfbf1cc6ae
Mystical Apple <timon.cappellapimor@gmail.com>
2021-05-14 02:29:14 -0700

    please work

---
+++ window.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+window = Tk()
+
+window.title(input())
+window.configure(width=500, height=300)
+window.configure(bg='lightgray')
+
+# move window center
+winWidth = window.winfo_reqwidth()
+winwHeight = window.winfo_reqheight()
+posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
+posDown = int(window.winfo_screenheight() / 2 - winwHeight / 2)
+window.geometry("+{}+{}".format(posRight, posDown))
+
+window.mainloop()
