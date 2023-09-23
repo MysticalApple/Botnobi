3d6de1f90c2c8adc1ab287d88f451b51f6f09323
MysticalApple <timon.cappellapimor@gmail.com>
2023-09-22 21:55:08 -0700

    oop forgot to commit util.py :skull:

--- utils/util.py
+++ utils/util.py
@@ -2,6 +2,7 @@ import json
 
 config_path = "config.json"
 
+# Configs
 def config_get(option):
     with open(config_path, "r") as config_file:
         config = json.load(config_file)
@@ -27,6 +28,30 @@ def config_set(option, value):
         print("config set incorrectly i think")
 
 
+# Github Feed List
+def get_feeds_from_file(file):
+    """
+    Reads in a list of tracked feeds
+    """
+    feeds = []
+    with open(file, "r") as feeds_file:
+        for entry in feeds_file.read().strip().split("\n"):
+            feed = {"link": entry.split(" ")[0], 
+                    "commits": entry.split(" ")[1:]}
+            feeds.append(feed)
+
+    return feeds
+
+def write_feeds_to_file(file, feeds):
+    """
+    Writes a list of tracked feeds to a text file
+    """
+    with open(file, "w") as feeds_file:
+        for feed in feeds:
+            feeds_file.write(f"{feed['link']} {' '.join(feed['commits'])}\n")
+
+
+# Eval Command
 def clean_code(content):
     if content.startswith("```") and content.endswith("```"):
         return "\n".join(content.split("\n")[1:][:-1])
